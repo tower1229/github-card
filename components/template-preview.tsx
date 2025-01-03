61553401f701521d6d7f030c03b8b49d176ce8d0
tower1229 <tower1229@foxmail.com>
2025-01-03 17:23:31 +0800

    Refactor InputUsername and TemplatePreview components: Simplified class names and layout structure for improved readability and consistency. Added BorderBeam component to enhance visual presentation in TemplatePreview.

--- components/template-preview.tsx
+++ components/template-preview.tsx
@@ -6,19 +6,21 @@ import Image from "next/image";
 import PreviewLinktree from "@/public/preview/linktree.png";
 import PreviewContribute from "@/public/preview/contribute.png";
 import { useState } from "react";
+import { BorderBeam } from "@/components/ui/border-beam";
 
 export function TemplatePreview() {
   const [template, setTemplate] = useState(Templates[0].value);
   const preview = template === "linktree" ? PreviewLinktree : PreviewContribute;
 
   return (
-    <>
-      <div className="flex gap-8 flex-col sm:flex-row">
+    <div className="shadow-xl relative bg-white/40 p-4 rounded-2xl ">
+      <div className="flex flex-col sm:flex-row  overflow-hidden rounded-lg">
         <InputUsername onTemplateChange={setTemplate} />
-        <div className="rounded-lg overflow-hidden shadow-xl w-[335px] h-[504px] relative">
+        <div className="w-[320px] sm:w-[335px] h-[504px] relative">
           <Image src={preview} alt="Preview" className="w-full" />
         </div>
       </div>
-    </>
+      <BorderBeam size={250} duration={10} delay={9} />
+    </div>
   );
 }
